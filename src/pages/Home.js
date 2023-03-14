d930511ae18c75aeab7496bfcb4afc7d04a4850c
patry <Rozowyjednorozec135@gmail.com>
2023-03-14 17:11:45 +0100

    renamed routs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,10 @@
+import logo from './logo.png';
 import React from 'react';
+import './Home.css';
+
 export const Home = () => {
-    return <div> Siema 2</div>
+    return <div className={"Home-logo"}>
+        <img src={logo}/>
+    </div>
 }
+
