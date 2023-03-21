b9b55dd43271e7a6db5e0bc4ba796d8b1634db60
patry <Rozowyjednorozec135@gmail.com>
2023-03-21 12:21:46 +0100

    renamed routs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,23 @@
 import logo from './logo.png';
-import plamka from './plamka.png';
-import React from 'react';
+import plama2 from './plama2.png';
+import plama from './plamkatop.png';
 import './Home.css';
+import React from 'react';
+
 
 
 
 export const Home = () => {
-    return <div className="Home-logo">
-        <img src={logo}/>
-    </div>
-    }
-    <div className={"Home-plamka"}>
-        <img src={plamka}/>
+
+    return <div >
+        <div className="Home-logo">
+            <img src={logo}/>
+        </div>
+        <img className="plama2" src={plama2}/>
+        <img className="plama" src={p}/>
+
     </div>
 
-
+}
 
 
