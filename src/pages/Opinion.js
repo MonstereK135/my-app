b1c94dc22b7271c187fcb54e3f948d3b69eb976f
patry <Rozowyjednorozec135@gmail.com>
2023-03-28 19:36:06 +0200

    code basic

--- src/pages/Opinion.js
+++ src/pages/Opinion.js
@@ -1,37 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import plama2 from './plamabottom.png';
 import plama from './plamatop.png';
 import frame1 from './frame1.png';
 import './Opinion.css';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 import {Rate} from "antd";
+import { initializeApp } from 'firebase/app';
 export const Opinion = () => {
-    const data = [{
-        date: "8.02.2023",
-        opinion: "Lorem ipsum dolor sit amet consectetur. Eleifend id condimentum sed iaculis hendrerit eu quam. Condimentum egestas tortor tincidunt pretium gravida a. Laoreet amet nec ut amet ornare id feugiat. Elementum rhoncus ac dictum.",
-        email: "fsfsd@wp.lp",
-        rate: 2
-    },{
-        date: "2.02.1231",
-        opinion: "Lorem ipsum dolor sit amet consectetur. Eleifend id condimentum sed iaculis hendrerit eu quam. Condimentum egestas tortor tincidunt pretium gravida a. Laoreet amet nec ut amet ornare id feugiat. Elementum rhoncus ac dictum.",
-        email: "fsfsd@wp.lp",
-        rate: 4.5
-    }];
+    const [opinions,setOpinions]=useState([])
 
+    var firebaseConfig = {
+        projectId: "my-app-b2e32",
+    };
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+    useEffect(()=>{
+        (async () => {
+            const opinionsCol = collection(db, 'opinions');
+            const opinionsSnapshot = await getDocs(opinionsCol);
+            const opinionsList = opinionsSnapshot.docs.map(doc => doc.data());
+            setOpinions(opinionsList)
+        })()
+    },[])
     return <div>
         <img className="plama2" src={plama2}/>
         <img className="plama" src={plama}/>
         <p className="Opinie" >Opinie</p>
         {
-            data.map(el => <div className="Opinion-o">
+            opinions.map(el => <div className="Opinion-o">
                 <p className="data1" >{el.date}</p>
                 <p className="tekst" > {el.opinion}</p>
                 <div className="rate-opinion">
                     <p className="Ocena2" style={{height: 19, width: 46 }}> Ocena</p>
                     <Rate
+                        disabled
                         defaultValue={el.rate}
                         style={{fontSize: 30}}
                         allowHalf
-                        tooltips={["źle", "słabo", "średnio", "dobrze", "bardzo dobrze"]}
                         className="rate2"
                         Showcopywriting={1}/>
                 </div>
