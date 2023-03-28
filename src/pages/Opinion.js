6d7edd50fc93f30f1f40146da6f014382a9b4720
patry <Rozowyjednorozec135@gmail.com>
2023-03-28 15:19:48 +0200

    basic code

--- src/pages/Opinion.js
+++ src/pages/Opinion.js
@@ -21,38 +21,31 @@ export const Opinion = () => {
         <img className="plama2" src={plama2}/>
         <img className="plama" src={plama}/>
         <p className="Opinie" >Opinie</p>
-
-        <div className="Opinion-o">
-            <p className="data1" >31.12.2012</p>
-            <p className="tekst" > Lorem ipsum dolor sit amet consectetur. Eleifend id condimentum sed iaculis hendrerit eu quam. Condimentum egestas tortor tincidunt pretium gravida a. Laoreet amet nec ut amet ornare id feugiat. Elementum rhoncus ac dictum.</p>
-            <div className="rate-opinion">
-                <p className="Ocena2" style={{height: 19, width: 46 }}> Ocena</p>
-                <Rate
-                    style={{fontSize: 30}}
-                    allowHalf
-                    tooltips={["źle", "słabo", "średnio", "dobrze", "bardzo dobrze"]}
-                    className="rate2"
-                    Showcopywriting={1}/>
-            </div>
-            <div className="wiersz-email" style={{height: 40, }}>
-                <p className="Email-opinia" > Email</p>
-                <p> <a className="mailto" href="mailto:someone@example.com">Rozowyejdnrozec135@gmail.com</a></p>
-            </div>
-
-
-{/*            <img className="frame1" style={{ height: 240, width: "100%" }} src={frame1}/>
-            <img className="frame2" style={{ height: 240, width: "100%" }} src={frame1}/>
-            <img className="frame3" style={{ height: 240, width: "100%" }} src={frame1}/>*/}
-
-
+        {
+            data.map(el => <div className="Opinion-o">
+                <p className="data1" >{el.date}</p>
+                <p className="tekst" > {el.opinion}</p>
+                <div className="rate-opinion">
+                    <p className="Ocena2" style={{height: 19, width: 46 }}> Ocena</p>
+                    <Rate
+                        defaultValue={el.rate}
+                        style={{fontSize: 30}}
+                        allowHalf
+                        tooltips={["źle", "słabo", "średnio", "dobrze", "bardzo dobrze"]}
+                        className="rate2"
+                        Showcopywriting={1}/>
                 </div>
+                <div className="wiersz-email" style={{height: 40, }}>
+                    <p className="Email-opinia" > Email</p>
+                    <p> <a className="mailto" href={"mailto:"+el.email}>{el.email}</a></p>
+                </div>
+            </div>)
+        }
 
 
 
-        {/*      <p className="data1" >31.12.2012</p>
-        <p className="data2" >31.12.2012</p>
-        <p className="data3" >31.12.2012</p>
-        <p className="tekst" > Lorem ipsum dolor sit amet consectetur. Eleifend id condimentum sed iaculis hendrerit eu quam. Condimentum egestas tortor tincidunt pretium gravida a. Laoreet amet nec ut amet ornare id feugiat. Elementum rhoncus ac dictum.</p>*/}
+
+
     </div>
 }
 
